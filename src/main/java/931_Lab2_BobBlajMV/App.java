487ca8a020062346da1e951758f1e26cbd294971
Andrei Blaj <andreiblajdev@gmail.com>
2019-03-20 10:45:48 +0200

    Initial commit

---
+++ src/main/java/931_Lab2_BobBlajMV/App.java
@@ -0,0 +1,13 @@
+package 931_Lab2_BobBlajMV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/931_Lab2_BobBlajMV/AppTest.java
@@ -0,0 +1,20 @@
+package 931_Lab2_BobBlajMV;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
