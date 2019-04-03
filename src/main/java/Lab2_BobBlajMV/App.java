e93ec00d3611584ec8af0b774abe82762ba45f74
Andrei Blaj <andreiblajdev@gmail.com>
2019-04-03 11:33:35 +0300

    final commit lab 3 - 2

--- src/main/java/Lab2_BobBlajMV/App.java
+++ src/main/java/Lab2_BobBlajMV/App.java
@@ -8,17 +8,18 @@ import Lab2_BobBlajMV.validation.*;
 public class App 
 {
     public static void main( String[] args )
-    { Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/note2.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-        UI consola = new UI(service);
-        consola.run();
+    {
+//        Validator<Student> studentValidator = new StudentValidator();
+//        Validator<Tema> temaValidator = new TemaValidator();
+//        Validator<Nota> notaValidator = new NotaValidator();
+//
+//        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/studenti.xml");
+//        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/teme.xml");
+//        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/note2.xml");
+//
+//        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+//        UI consola = new UI(service);
+//        consola.run();
 
         //PENTRU GUI
         // de avut un check: daca profesorul introduce sau nu saptamana la timp
