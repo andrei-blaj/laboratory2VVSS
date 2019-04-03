06e72fb260b803a826f51ef4801cd5c28926c78d
Andrei Blaj <andreiblajdev@gmail.com>
2019-04-03 11:16:39 +0300

    .

--- src/test/java/Lab2_BobBlajMV/AppTest.java
+++ src/test/java/Lab2_BobBlajMV/AppTest.java
@@ -3,11 +3,14 @@ package Lab2_BobBlajMV;
 import static org.junit.Assert.assertTrue;
 
 import Lab2_BobBlajMV.domain.Student;
+import Lab2_BobBlajMV.domain.Tema;
 import Lab2_BobBlajMV.repository.StudentXMLRepository;
+import Lab2_BobBlajMV.repository.TemaXMLRepository;
 import Lab2_BobBlajMV.validation.ValidationException;
 import Lab2_BobBlajMV.validation.Validator;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -24,33 +27,74 @@ public class AppTest
         assertTrue( true );
     }
 
-    @Test
-    public void addStudentHappyFlow(){
-        Student student = new Student("7", "Bob", 931);
+//    @Test
+//    public void addStudentHappyFlow(){
+//        Student student = new Student("7", "Bob", 931);
+//
+//        Validator<Student> validator = new Validator<Student>() {
+//            @Override
+//            public void validate(Student entity) throws ValidationException {
+//
+//            }
+//        };
+//
+//        StudentXMLRepository fileRepository1 = new StudentXMLRepository(validator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/studenti.xml");
+//        fileRepository1.save(student);
+//    }
+//
+//    @Test
+//    public void addStudentError(){
+//        Student student = new Student("7", "Bob", 931);
+//
+//        Validator<Student> validator = new Validator<Student>() {
+//            @Override
+//            public void validate(Student entity) throws ValidationException {
+//
+//            }
+//        };
+//
+//        StudentXMLRepository fileRepository1 = new StudentXMLRepository(validator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/studenti.xml");
+//        fileRepository1.save(student);
+//    }
 
-        Validator<Student> validator = new Validator<Student>() {
+    @Test
+    public void addAssignment(){
+        Tema assignment = new Tema("6", "Description", 8, 2);
+
+        Validator<Tema> validator = new Validator<Tema>() {
             @Override
-            public void validate(Student entity) throws ValidationException {
+            public void validate(Tema entity) throws ValidationException {
 
             }
         };
 
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(validator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/studenti.xml");
-        fileRepository1.save(student);
+        TemaXMLRepository fileRepository = new TemaXMLRepository(validator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/teme.xml");
+        fileRepository.save(assignment);
+
+        Tema tema = fileRepository.findOne("6");
+
+        Assert.assertTrue(tema.getDeadline()==8);
     }
 
     @Test
-    public void addStudentError(){
-        Student student = new Student("4", "Bob", 931);
+    public void addAssignmentError(){
+        Tema assignment = new Tema("6", "Description", 8, 2);
+        Tema assignmentSecond = new Tema("6", "Description-second", 7, 4);
 
-        Validator<Student> validator = new Validator<Student>() {
+
+        Validator<Tema> validator = new Validator<Tema>() {
             @Override
-            public void validate(Student entity) throws ValidationException {
+            public void validate(Tema entity) throws ValidationException {
 
             }
         };
 
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(validator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/studenti.xml");
-        fileRepository1.save(student);
+        TemaXMLRepository fileRepository = new TemaXMLRepository(validator, "/Users/andrei/IdeaProjects/_931_Lab2_BobBlaj/src/main/java/Lab2_BobBlajMV/teme.xml");
+        fileRepository.save(assignment);
+        fileRepository.save(assignmentSecond);
+
+        Tema tema = fileRepository.findOne("6");
+
+        Assert.assertTrue(!(tema.getDeadline()==7));
     }
 }
